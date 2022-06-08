3f49ddbc6f61124ffa5b70af05df76bf495e4df9
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-06-08 15:45:22 -0300

    ADDED: Flame Splash Screen. (#20)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bird/widgets/app.dart';
+import 'package:flutter_bird/widgets/flutter_bird_app.dart';
 
 void main() => runApp(const FlutterBirdApp());
